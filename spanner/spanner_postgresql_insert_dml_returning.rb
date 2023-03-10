11802010ef10e42c025368b8bb781a81fa88d598
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2023-03-11 01:42:43 +0530

    Update spanner_postgresql_insert_dml_returning.rb

--- spanner/spanner_postgresql_insert_dml_returning.rb
+++ spanner/spanner_postgresql_insert_dml_returning.rb
@@ -27,10 +27,15 @@ def spanner_postgresql_insert_dml_returning project_id:, instance_id:, database_
   spanner = Google::Cloud::Spanner.new project: project_id
   client = spanner.client instance_id, database_id
 
+  # Insert records into SINGERS table and returns the
+  # generated column FullName of the inserted records
+  # using ‘RETURNING FullName’.
+  # It is also possible to return all columns of all the
+  # inserted records by using ‘RETURNING *’.
   client.transaction do |transaction|
-    results = transaction.execute_query "INSERT INTO Singers (SingerId, FirstName, LastName) VALUES (12, 'Melissa', 'Garcia'), (13, 'Russell', 'Morales'), (14, 'Jacqueline', 'Long'), (15, 'Dylan', 'Shaw') RETURNING *"
+    results = transaction.execute_query "INSERT INTO Singers (SingerId, FirstName, LastName) VALUES (12, 'Melissa', 'Garcia'), (13, 'Russell', 'Morales'), (14, 'Jacqueline', 'Long'), (15, 'Dylan', 'Shaw') RETURNING FullName"
     results.rows.each do |row|
-      puts "Inserted singers with id: #{row[:singerid]}, FirstName: #{row[:firstname]}, LastName: #{row[:lastname]}"
+      puts "FullName: #{row[:fullname]}"
     end
     puts "Inserted row(s) count: #{results.row_count}"
   end
